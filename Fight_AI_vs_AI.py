4d493f50e8c5b3daf63349d80a265e2380bf3a24
XxAC130xX <xsuteme1x@gmail.com>
2018-06-24 23:19:11 +0900

    add_ai_vs_as

---
+++ Fight_AI_vs_AI.py
@@ -0,0 +1,79 @@
+
+import argparse
+
+from Reversi import Reversi
+from dqn_agent import DQNAgent
+
+
+if __name__ == "__main__":
+    # args
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_path")
+    parser.add_argument("-s", "--save", dest="save", action="store_true")
+    parser.set_defaults(save=False)
+    args = parser.parse_args()
+
+    # environmet, agent
+    env = Reversi()
+    agent1 = DQNAgent(env.enable_actions, env.name, env.screen_n_rows, env.screen_n_cols)
+    #agent1.load_model("models1/Reversi.ckpt")
+
+    agent2 = DQNAgent(env.enable_actions, env.name, env.screen_n_rows, env.screen_n_cols)
+    agent2.load_model("models2/Reversi.ckpt")
+
+
+    # game
+    print("------------- GAME START ---------------")
+    while not env.isEnd():
+        # print("*** userターン○ ***")
+        # env.print_screen()
+        # enables = env.get_enables(1)
+        # if len(enables) > 0:
+        #     flg = False
+        #     while not flg:
+        #         print("番号を入力してください")
+        #         print(enables)
+        #         inp = input('>>>  ')
+        #         action_t = int(inp)
+        #         for i in enables:                
+        #             if action_t == i:
+        #                 flg = True                       
+        #                 break
+                
+        #     env.update(action_t, 1)
+        # else:
+        #     print("パス")
+            
+            
+        #if env.isEnd() == True:break
+        
+        print("*** AI1ターン●  ***")
+        env.print_screen()
+        enables = env.get_enables(1)
+        if len(enables) > 0:
+            qvalue, action_t = agent1.select_enable_action(env.screen, enables)
+            print('>>>  {:}'.format(action_t))              
+            env.update(action_t, 1)
+        else:
+            print("パス")
+
+        key = input("Next")
+ 
+        print("*** AI2ターン○ ***")
+        env.print_screen()
+        enables = env.get_enables(2)
+        if len(enables) > 0:
+            qvalue, action_t = agent2.select_enable_action(env.screen, enables)
+            print('>>>  {:}'.format(action_t))              
+            env.update(action_t, 2)
+        else:
+            print("パス")
+
+        ket = input("Next")
+
+    print("*** ゲーム終了 ***")
+    if env.winner() == 1:
+        print("AI1の勝ち！ AI1スコアは、{:}です。".format(env.get_score(1)))
+    else:
+        print("AI2の勝ち！ AI2のスコアは、{:}です。".format(env.get_score(2)))
+    
---
+++ train.py
@@ -0,0 +1,88 @@
+import copy
+
+from Reversi import Reversi
+from dqn_agent import DQNAgent
+
+          
+if __name__ == "__main__":
+    
+    # parameters
+    n_epochs = 100
+    # environment, agent
+    env = Reversi()
+ 
+    # playerID    
+    playerID = [env.Black, env.White, env.Black]
+
+    # player agent    
+    players = []
+    # player[0]= env.Black
+    players.append(DQNAgent(env.enable_actions, env.name, env.screen_n_rows, env.screen_n_cols))
+    # player[1]= env.White
+    players.append(DQNAgent(env.enable_actions, env.name, env.screen_n_rows, env.screen_n_cols))
+   
+    
+    for e in range(n_epochs):
+        # reset
+        env.reset()
+        terminal = False
+        while terminal == False: # 1エピソードが終わるまでループ
+
+            for i in range(0, len(players)): 
+                
+                state = env.screen
+                targets = env.get_enables(playerID[i])
+                
+                if len(targets) > 0:
+                    # どこかに置く場所がある場合 
+
+                    #すべての手をトレーニングする
+                    for tr in targets:
+                        tmp = copy.deepcopy(env)
+                        tmp.update(tr, playerID[i])
+                        #終了判定
+                        win = tmp.winner()
+                        end = tmp.isEnd()
+                        #次の状態
+                        state_X = tmp.screen
+                        target_X = tmp.get_enables(playerID[i+1])
+                        if len(target_X) == 0:
+                            target_X = tmp.get_enables(playerID[i])
+
+                        # 両者トレーニング
+                        for j in range(0, len(players)):
+                            reword = 0
+                            if end == True:
+                                if win == playerID[j]:
+                                    # 勝ったら報酬1を得る
+                                    reword = 1
+                           
+                            players[j].store_experience(state, targets, tr, reword, state_X, target_X, end)
+                            players[j].experience_replay()
+
+
+                    # 行動を選択  
+                    action = players[i].select_action(state, targets, players[i].exploration)
+                    # 行動を実行
+                    env.update(action, playerID[i])
+                    # for log
+                    loss = players[i].current_loss
+                    Q_max, Q_action = players[i].select_enable_action(state, targets)
+                    print("player:{:1d} | pos:{:2d} | LOSS: {:.4f} | Q_MAX: {:.4f}".format(
+                             playerID[i], action, loss, Q_max))
+                     
+
+  
+
+                # 行動を実行した結果
+                terminal = env.isEnd()     
+                              
+        w = env.winner()                    
+        print("EPOCH: {:03d}/{:03d} | WIN: player{:1d}".format(
+                         e, n_epochs, w))
+
+
+    # 保存は後攻のplayer1 を保存する。
+    players[0].save_model()
+
+           
